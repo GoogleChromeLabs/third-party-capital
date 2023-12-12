2b12916515a07a0081f726a19d3951990b626bde
Felix Arntz <felixarntz@google.com>
2023-12-11 17:18:05 -0800

    Implement the actual third party classes.

---
+++ inc/Contracts/ThirdParty.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Interface GoogleChromeLabs\ThirdPartyCapital\Contracts\ThirdParty
+ *
+ * @package   Third Party Capital
+ * @copyright 2023 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ */
+
+namespace GoogleChromeLabs\ThirdPartyCapital\Contracts;
+
+use GoogleChromeLabs\ThirdPartyCapital\Data\ThirdPartyScriptOutput;
+
+/**
+ * Interface for a class representing a third party integration.
+ */
+interface ThirdParty
+{
+
+    /**
+     * Sets input arguments for the integration.
+     *
+     * @param array $args Input arguments to set.
+     */
+    public function setArgs(array $args);
+
+    /**
+     * Gets the HTML output for the integration.
+     *
+     * Only relevant if the integration provides user-facing output.
+     *
+     * @return string HTML output, or empty string if not applicable.
+     */
+    public function getHtml(): string;
+
+    /**
+     * Gets the stylesheet URLs for the integration.
+     *
+     * Only relevant if the integration provides stylesheets to use.
+     *
+     * @return string[] List of stylesheet URLs, or empty array if not applicable.
+     */
+    public function getStylesheets(): array;
+
+    /**
+     * Gets the script definitions for the integration.
+     *
+     * Only relevant if the integration provides scripts to use.
+     *
+     * @return ThirdPartyScriptOutput[] List of script definition objects, or empty array if not applicable.
+     */
+    public function getScripts(): array;
+}
---
+++ inc/ThirdParties/ThirdPartyBase.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Class GoogleChromeLabs\ThirdPartyCapital\ThirdParties\ThirdPartyBase
+ *
+ * @package   Third Party Capital
+ * @copyright 2023 Google LLC
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ */
+
+namespace GoogleChromeLabs\ThirdPartyCapital\ThirdParties;
+
+use GoogleChromeLabs\ThirdPartyCapital\Contracts\ThirdParty;
+use GoogleChromeLabs\ThirdPartyCapital\Data\ThirdPartyData;
+use GoogleChromeLabs\ThirdPartyCapital\Data\ThirdPartyDataFormatter;
+use GoogleChromeLabs\ThirdPartyCapital\Data\ThirdPartyOutput;
+use GoogleChromeLabs\ThirdPartyCapital\Data\ThirdPartyScriptOutput;
+
+/**
+ * Base class representing a third party integration.
+ */
+abstract class ThirdPartyBase implements ThirdParty
+{
+    /**
+     * Absolute path to the third party JSON file.
+     *
+     * @var string
+     */
+    private $jsonFilePath;
+
+    /**
+     * Input arguments.
+     *
+     * @var array
+     */
+    private $args = array();
+
+    /**
+     * Third party data instance, lazily initialized.
+     *
+     * @var ?ThirdPartyData
+     */
+    private $data;
+
+    /**
+     * Third party output instance, lazily initialized.
+     *
+     * @var ?ThirdPartyOutput
+     */
+    private $output;
+
+    /**
+     * Constructor.
+     *
+     * @param array $args Input arguments to set.
+     */
+    public function __construct(array $args)
+    {
+        $this->jsonFilePath = $this->getJsonFilePath();
+
+        $this->setArgs($args);
+    }
+
+    /**
+     * Sets input arguments for the integration.
+     *
+     * @param array $args Input arguments to set.
+     */
+    public function setArgs(array $args)
+    {
+        $this->args = $args;
+
+        // Reset third party output.
+        $this->output = null;
+    }
+
+    /**
+     * Gets the HTML output for the integration.
+     *
+     * Only relevant if the integration provides user-facing output.
+     *
+     * @return string HTML output, or empty string if not applicable.
+     */
+    public function getHtml(): string
+    {
+        $this->lazilyInitialize();
+
+        return $this->output->getHtml();
+    }
+
+    /**
+     * Gets the stylesheet URLs for the integration.
+     *
+     * Only relevant if the integration provides stylesheets to use.
+     *
+     * @return string[] List of stylesheet URLs, or empty array if not applicable.
+     */
+    public function getStylesheets(): array
+    {
+        $this->lazilyInitialize();
+
+        return $this->output->getStylesheets();
+    }
+
+    /**
+     * Gets the script definitions for the integration.
+     *
+     * Only relevant if the integration provides scripts to use.
+     *
+     * @return ThirdPartyScriptOutput[] List of script definition objects, or empty array if not applicable.
+     */
+    public function getScripts(): array
+    {
+        $this->lazilyInitialize();
+
+        return $this->output->getScripts();
+    }
+
+    /**
+     * Gets the path to the third party data JSON file.
+     *
+     * @return string Absolute path to the JSON file.
+     */
+    abstract protected function getJsonFilePath(): string;
+
+    /**
+     * Lazily initializes the data and output instances, only if they aren't initialized yet.
+     *
+     * The data instance is only initialized once as it is agnostic to the input arguments.
+     * The output instance needs to be reinitialized whenever the input arguments change.
+     */
+    private function lazilyInitialize()
+    {
+        if (! $this->data) {
+            $this->data = ThirdPartyData::fromJsonFile($this->jsonFilePath);
+        }
+
+        if (! $this->output) {
+            $this->output = ThirdPartyDataFormatter::formatData($this->data, $this->args);
+        }
+    }
+}
